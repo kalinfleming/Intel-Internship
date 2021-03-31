08db55d70d82462e7c2fea15dabd7482bf08dbeb
kalinfleming <flemkk19@wfu.edu>
2021-03-31 15:04:38 -0400

    adding customer vs administraotr

--- airline.cs
+++ airline.cs
@@ -33,28 +33,40 @@ namespace Intel_Internship
             string five;
             string six;
             string seven;
-            Console.Write("Enter a seating capacity - ");
-            one = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter price of 1st class ticket - ");
-            two = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter price of economy ticket - ");
-            three = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter departure airport - ");
-            four = Console.ReadLine();
-            Console.Write("Enter arrival airport - ");
-            five = Console.ReadLine();
-            Console.Write("Enter departure time - ");
-            six = Console.ReadLine();
-            Console.Write("Enter arrival time - ");
-            seven = Console.ReadLine();
-            Flight myObj = new Flight(one, two, three, four, five, six, seven);
-            Console.WriteLine("Flight Capacity: {0}", myObj.seatingCapacity);
-            Console.WriteLine("Price of First Class Ticket: {0}", myObj.pFirst);
-            Console.WriteLine("Price of Economy Ticket: {0}", myObj.pEconomy);
-            Console.WriteLine("Departure Airport: {0}", myObj.dAirport);
-            Console.WriteLine("Arrival Airport: {0}", myObj.aAirport);
-            Console.WriteLine("Departure Time: {0}", myObj.dTime);
-            Console.WriteLine("Arrival Time: {0}", myObj.aTime);
+            string person;
+
+            Console.Write("Hello! Welcome to the Booking Portal. Are you a customer or administrator? ");
+            person = Console.ReadLine();
+            person = person.ToLower();
+
+            if(person.Equals("administrator")) 
+            {
+                Console.Write("Enter a seating capacity - ");
+                one = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter price of 1st class ticket - ");
+                two = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter price of economy ticket - ");
+                three = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter departure airport - ");
+                four = Console.ReadLine();
+                Console.Write("Enter arrival airport - ");
+                five = Console.ReadLine();
+                Console.Write("Enter departure time - ");
+                six = Console.ReadLine();
+                Console.Write("Enter arrival time - ");
+                seven = Console.ReadLine();
+                Flight myObj = new Flight(one, two, three, four, five, six, seven);
+                Console.WriteLine("Flight Capacity: {0}", myObj.seatingCapacity);
+                Console.WriteLine("Price of First Class Ticket: {0}", myObj.pFirst);
+                Console.WriteLine("Price of Economy Ticket: {0}", myObj.pEconomy);
+                Console.WriteLine("Departure Airport: {0}", myObj.dAirport);
+                Console.WriteLine("Arrival Airport: {0}", myObj.aAirport);
+                Console.WriteLine("Departure Time: {0}", myObj.dTime);
+                Console.WriteLine("Arrival Time: {0}", myObj.aTime);
+            }
+            else {
+                Console.WriteLine("Please come back later");
+            }
         }
     }
 }
