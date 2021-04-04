7fc5a8a027fb4803f3bd1e4d0a3f2f7ac6b546b6
kalinfleming <flemkk19@wfu.edu>
2021-04-04 15:01:08 -0400

    Adding cutomer and database

--- airline.cs
+++ airline.cs
@@ -1,7 +1,26 @@
 ﻿using System;
+using System.Collections;
 
 namespace Intel_Internship
 {
+    public class Database
+    {
+        public int numFlights;
+        public ArrayList database;
+        public Database()
+        {
+            numFlights = 0;
+            database = new ArrayList();
+        }
+        public void addFlight(Flight f)
+        {
+            database.Add(f);
+            numFlights++;
+            Console.WriteLine("Number of flights: {0}", numFlights);
+        }
+
+    }
+    
     public class Flight
     {
                 public int seatingCapacity;
@@ -23,6 +42,8 @@ namespace Intel_Internship
                 }
                 public Flight(int sc, int pf, int pe, string da, string aa, string dt, string at)
                 {
+                    da = da.ToUpper();
+                    aa = aa.ToUpper();
                     seatingCapacity = sc;
                     pFirst = pf;
                     pEconomy = pe;
@@ -48,6 +69,7 @@ namespace Intel_Internship
             age = a;
         }
     }
+    
     class Airline
     {
         static void Main(string[] args)
@@ -60,28 +82,45 @@ namespace Intel_Internship
             string six;
             string seven;
             string person;
+            string cName;
+            int cAge;
+            string answer;
+            int prompt = 0;
 
             Console.Write("Hello! Welcome to the Booking Portal. Are you a customer or administrator? ");
             person = Console.ReadLine();
             person = person.ToLower();
+            
+            while (prompt == 0) {
+                if ((person.Equals("administrator")) || (person.Equals("customer"))) {
+                    prompt = 1;
+                } else {
+                    Console.WriteLine("Please check your spelling and try again.");
+                    Console.Write("Hello! Welcome to the Booking Portal. Are you a customer or administrator? ");
+                    person = Console.ReadLine();
+                    person = person.ToLower();
+                }
+            }
 
             if(person.Equals("administrator")) 
             {
                 Console.Write("Enter a seating capacity - ");
                 one = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter price of 1st class ticket - ");
+                Console.Write("Enter price of 1st class ticket - $");
                 two = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter price of economy ticket - ");
+                Console.Write("Enter price of economy ticket - $");
                 three = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Enter departure airport - ");
                 four = Console.ReadLine();
                 Console.Write("Enter arrival airport - ");
                 five = Console.ReadLine();
-                Console.Write("Enter departure time - ");
+                Console.Write("Enter departure time (XX:XX) - ");
                 six = Console.ReadLine();
-                Console.Write("Enter arrival time - ");
+                Console.Write("Enter arrival time (XX:XX) - ");
                 seven = Console.ReadLine();
+                Database d = new Database();
                 Flight myObj = new Flight(one, two, three, four, five, six, seven);
+                d.addFlight(myObj);
                 Console.WriteLine("Flight Capacity: {0}", myObj.seatingCapacity);
                 Console.WriteLine("Price of First Class Ticket: {0}", myObj.pFirst);
                 Console.WriteLine("Price of Economy Ticket: {0}", myObj.pEconomy);
@@ -90,8 +129,23 @@ namespace Intel_Internship
                 Console.WriteLine("Departure Time: {0}", myObj.dTime);
                 Console.WriteLine("Arrival Time: {0}", myObj.aTime);
             }
-            else {
-                Console.WriteLine("Please come back later");
+            else if(person.Equals("customer")) {
+                Console.Write("Please enter your first and last name - ");
+                cName = Console.ReadLine();
+                Console.Write("Please enter your age - ");
+                cAge = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Would you like to see a list of the flights (Y/N) - ");
+                answer = Console.ReadLine();
+                bool areEqual = String.Equals("Y", answer, StringComparison.OrdinalIgnoreCase);
+                if (areEqual) {
+                    Console.WriteLine("List of flights");
+                }
+                else {
+                    bool areEqual2 = String.Equals("N", answer, StringComparison.OrdinalIgnoreCase);
+                    if (areEqual2) {
+                        Console.WriteLine("Please come again. Have a great day!");
+                    }
+                }
             }
         }
     }
