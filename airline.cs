afe27383a9c1f08954bb2aeb80058f2295190f1b
kalinfleming <flemkk19@wfu.edu>
2021-04-09 12:19:26 -0400

    updating README

--- airline.cs
+++ airline.cs
@@ -2,12 +2,6 @@
 Kalin Fleming
 Intel Internship Airline Project
 04/09/2021
-Description: This program simulates an Airline website. The user can either be a customer or an administrator. The administrator
-can create flights by inputting different flight descriptions. The program will store all flights into a text file named
-"flights.txt". The administrator can also clear any previous reservations made by customers for all flights.
-The customer can reserve a seat on a flight. The customer will be able to see a list of flights available
-with the relevant descriptions and reserve a seat. If customers attempt to book the same seat in the same program simulation,
-the customer will have to choose a different seat. 
 */
 
 /* import files */
